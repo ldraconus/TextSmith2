53526032be4ef13741131ed6ce114bd08fce9b90
chris <chris.m.olson@gmail.com>
2026-04-02 23:39:01 -0500

    Fixed to make text conversion obey width/height
    After calculating from page width/height. Maybe work on adding marginals
    in the future (in support of standard submission guidelines).

--- TextExporter.cpp
+++ TextExporter.cpp
@@ -15,15 +15,64 @@ bool TextExporter::convert() {
     QString ext =  "." + (info.completeSuffix().isEmpty() ? Extension() : info.completeSuffix());
     Main::ref().setDocDir(path);
 
+    auto pointMargins = Main::ref().prefs().margins(Preferences::Units::Points);
+    QSize pageSize(8.5 * 72, 11 * 72);
+    int pointWidth = pageSize.width() - pointMargins[Preferences::Left] - pointMargins[Preferences::Right];
+    int pointHeight = pageSize.height() - pointMargins[Preferences::Top] - pointMargins[Preferences::Bottom];
+
+    QFont courier("Courier", 12);
+    QFontMetrics metrics(courier);
+
+    auto charWidth = metrics.averageCharWidth();
+    QScreen* widgetScreen = Main::ptr()->screen();
+    qreal dpiX = 1;
+    if (widgetScreen)  dpiX = widgetScreen->logicalDotsPerInchX();
+    int fontHeight = 12;
+    int fontWidth = charWidth / dpiX;
+
+    int linesPerPage = pointHeight / fontHeight;
+    int charactersPerLine = pointWidth / fontWidth;
+
     QString dir = path;
 
     QString text;
+    int line = 0;
+    bool firstLineEver = true;
+    StringList chapterTags { Main::ref().prefs().chapterTag().split(Preferences::sep) };
+    StringList coverTags { Main::ref().prefs().coverTag().split(Preferences::sep) };
+    StringList sceneTags { Main::ref().prefs().sceneTag().split(Preferences::sep) };
     for (auto&& id: mItemIds) {
         Item& item = Main::ref().novel().findItem(id);
-        text += item.doc()->toPlainText() + "\n";
+        StringList paragraphs { item.doc()->toPlainText().split("\n") };
+        if (item.hasTag(coverTags)) continue;
+        if (item.hasTag(Main::ref().prefs().chapterTag())) {
+            if (firstLineEver) firstLineEver = false;
+            else {
+                text += QChar(12);
+                line = 0;
+            }
+        }
+        for (const auto& paragraph: paragraphs) {
+            text += "    ";
+            int width = 4;
+            StringList words { paragraph.split(" ") };
+            for (const auto& word: words) {
+                if (word.length() + 1 + width >= charactersPerLine) {
+                    text += "\n";
+                    ++line;
+                    if (line == linesPerPage) {
+                        text += QChar(12);
+                        line = 0;
+                    }
+                    text += word;
+                    width = word.length();
+                } else {
+                    text += " " + word;
+                    width += word.length();
+                }
+            }
+        }
     }
-
-    // go through text and assume courier 12, break at margins
 
     QFile file(dir + "/" + base + ext);
     if (file.open(QIODeviceBase::WriteOnly)) return file.write(text.toUtf8()) != -1;
