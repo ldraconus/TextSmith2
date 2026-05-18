f18f1eb0ca9426ff3b084617f2029f4aa32a8306
Chris Olson <chris.m.olson@gmail.com>
2026-05-18 13:12:21 -0500

    Finally stompde out the windows trace window 'bug'
    - not sure why it became a problem when I switched to 6.11, but it did.
    moving on.

--- Start.cpp
+++ Start.cpp
@@ -3,8 +3,16 @@
 #include <QApplication>
 
 int main(int argc, char *argv[]) {
-    qInstallMessageHandler([](QtMsgType, const QMessageLogContext&, const QString&){}); // in the future add compile flags to allow/disallow certain messages
     QApplication a(argc, argv);
+
+#ifdef QT_NO_DEBUG
+    qInstallMessageHandler([](QtMsgType, const QMessageLogContext&, const QString&){}); // in the future add compile flags to allow/disallow certain messages
+#ifdef Q_OS_WIN
+    FILE* nul = nullptr;
+    freopen_s(&nul, "NUL", "w", stderr);
+#endif
+#endif
+
     Main w(&a);
     w.show();
     return a.exec();
